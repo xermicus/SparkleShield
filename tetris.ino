8a85ed084699e86bc4067a530b96592d8009647e
Cyrill Leutwiler <bigcyrill@hotmail.com>
2015-12-28 21:21:38 +0100

    inital commit

---
+++ tetris.ino
@@ -0,0 +1,87 @@
+/*
+  FirstLight.ino - Library for controlling the Sparkle Shield LED display.
+  Created by Scotty Allen, December 24, 2015.
+  Released under a GPL v2 license.
+*/
+
+// Light up each LED, one after another, starting at LED1, ending at LED70.
+
+#include <FastLED.h>
+#include <SparkleShield.h>
+
+#define BRIGHTNESS 16
+
+#define RED CRGB(255, 0, 0)     
+#define GREEN CRGB(0, 255, 0)
+#define BLUE CRGB(0, 0, 255)      // 4er Balken
+#define YELLOW CRGB(255, 255, 0)  // Quadrat
+#define WHITE CHSV(0, 0, 0)
+
+CRGB field[10][7];
+
+SparkleShield sparkle;
+
+void spawn_element(CRGB element) {
+  if (element == YELLOW) {
+    for (int i = 0; i < 2; i++){
+      for (int j = 3; j < 5; j++) {       // insert random here (0..5, 2..7)
+        field[i][j] = element;
+        sparkle.set(i, j, field[i][j]);
+      }      
+    }
+  }
+}
+
+void update_field() {
+  CRGB cp_field[10][7] = field;
+  
+  for (int i = 1; i < 9; i++) {
+    for (int j = 0; j < 7; j++) {
+      // YELLOW
+      if (cp_field[i][j] == YELLOW && cp_field[i+1][j] == WHITE) {
+        field[i+1][j] = YELLOW;
+        field[i-1][j] = WHITE;
+
+      // BLUE
+
+      // GREEN
+      }
+    }
+  }
+}
+
+void draw_field() {
+  for (int i = 0; i < 10; i++) {
+    for (int j = 0; j < 7; j++) {
+      if (field[i][j] != WHITE) {
+        sparkle.set(i, j, field[i][j]);
+      }
+      else {
+        sparkle.set(i, j, WHITE);
+      }
+    }
+  }
+}
+
+void loop() {
+  update_field();
+
+  draw_field();
+  
+  sparkle.show();
+  delay(500);  
+}
+
+void setup() {
+  sparkle.setBrightness(BRIGHTNESS);
+
+  for(int i = 0; i < 10; i++) {
+    for(int j = 0; j < 7; j++) {
+      field[i][j] = WHITE;
+      sparkle.set(i, j, WHITE);
+    }
+  }
+  
+  spawn_element(YELLOW);
+  sparkle.show();
+}
