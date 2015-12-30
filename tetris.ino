0607c8648b352a825ae4324258bc40e0b51e07f6
Cyrill Leutwiler <bigcyrill@hotmail.com>
2015-12-30 02:22:40 +0100

    collision detection

--- tetris.ino
+++ tetris.ino
@@ -3,30 +3,55 @@
 
 #define BRIGHTNESS 16
 
-#define RED CRGB(255, 0, 0) 
-#define RED_BOTTOM CRGB(255, 0, 255)     
-#define GREEN CRGB(0, 255, 0)
+#define RED CRGB(255, 0, 0)           //Podest
+#define GREEN CRGB(0, 255, 0)     // L
 #define BLUE CRGB(0, 0, 255)      // 4er Balken
 #define YELLOW CRGB(255, 255, 0)  // Quadrat
-#define YELLOW_BOTTOM CRGB(255, 255, 128)
+#define GREY CRGB(128, 128, 128)  // J
 //#define WHITE CHSV(0, 0, 64)
 #define WHITE CRGB(0, 0, 0)
 
+int block[4][2];
+CRGB MOVING;
+
 CRGB field[10][7];
-int x_move;
-int y_move;
+//CRGB cp_field[10][7];
+
 
 SparkleShield sparkle;
 
 void spawn_element(CRGB element) {
   int randx = 4;
-  if (element == YELLOW) {    
-    for (int i = 0; i < 2; i++){
-      for (int j = randx; j < randx+2 ; j++) {       // insert random here (0..5, 2..7)
-        field[i][j] = element;
-        sparkle.set(i, j, field[i][j]);
-      }      
+  MOVING = element;  
+  if (element == GREY) {
+    block[0][0] = 0;
+    block[0][1] = randx+1;
+    block[1][0] = 1;
+    block[1][1] = randx+1;
+    block[2][0] = 2;
+    block[2][1] = randx+1;
+    block[3][0] = 2;
+    block[3][1] = randx;
+
+    for (int i = 0; i < 4; i++) {
+      //field[block[i][0]][block[i][1]] = element;
+      sparkle.set(block[i][0], block[i][1], element);
     }
+  }
+  if (element == YELLOW) {
+    block[0][0] = 0;
+    block[0][1] = randx;
+    block[1][0] = 1;
+    block[1][1] = randx;
+    block[2][0] = 0;
+    block[2][1] = randx+1;
+    block[3][0] = 1;
+    block[3][1] = randx+1;
+
+    for (int i = 0; i < 4; i++) {
+      //field[block[i][0]][block[i][1]] = element;
+      sparkle.set(block[i][0], block[i][1], element);
+    } 
   }
   else if (element == RED) { 
     int randx = 1;
@@ -38,6 +63,28 @@ void spawn_element(CRGB element) {
     sparkle.set(1, randx+1, element); 
     field[1][randx+2] = element;
     sparkle.set(1, randx+2, element); 
+  }
+  else if (element == BLUE) { 
+    int randx = 1;
+    field[0][randx] = element;
+    sparkle.set(0, randx, element); 
+    field[1][randx] = element;
+    sparkle.set(1, randx, element); 
+    field[2][randx] = element;
+    sparkle.set(2, randx, element); 
+    field[3][randx] = element;
+    sparkle.set(3, randx, element); 
+  }
+  else if (element == GREEN) { 
+    int randx = 1;
+    field[0][randx] = element;
+    sparkle.set(0, randx, element); 
+    field[1][randx] = element;
+    sparkle.set(1, randx, element); 
+    field[2][randx] = element;
+    sparkle.set(2, randx, element); 
+    field[2][randx+1] = element;
+    sparkle.set(2, randx+1, element); 
   }
 }
 
@@ -53,26 +100,43 @@ int calc_bottom(int y) {
 }
 
 
-void update_field() {
+void update_field() {  
   CRGB cp_field[10][7] = field;
-  int i_bottom;
-  for (int i = 0; i < 9; i++) {
-    for (int j = 0; j < 7; j++) {
-      i_bottom = calc_bottom(j);
-      if (cp_field[i][j] != WHITE && i < i_bottom) {
-        field[i+1][j] = cp_field[i][j];
-        sparkle.set(i+1, j, cp_field[i][j]);
-        if(cp_field[i-1][j] == WHITE || i < 1) {
-          field[i][j] = WHITE;
-          sparkle.set(i, j, WHITE);
-        }
-        // Spawn new
-        if(i+1 == i_bottom) {
-          spawn_element(YELLOW);
-        }
-      }
+  int i_bottom = 0;
+  bool b_move = true;
+
+  for (int i = 0; i < 4; i++) {
+    if (cp_field[block[i][0]+1][block[i][1]] != WHITE) {
+      b_move = false;
     }
   }
+
+  if (b_move)
+  {
+    for (int i = 0; i < 4; i++) {
+      sparkle.set(block[i][0]+1, block[i][1], MOVING);
+      block[i][0]++;
+      bool b_top = true;
+      //int i_top = block[i][0];
+      for (int j = 0; j < 4; j++){
+        if (block[j][0] ==  block[i][0]-1 && block[j][1] == block[i][1]) {
+          //i_top = block[j][0];
+          b_top = false;
+        }
+      }
+      if(b_top) {
+        sparkle.set(block[i][0]-1, block[i][1], WHITE);
+      }
+    }
+    
+  }
+  else {
+    for (int i = 0; i < 4; i++) {
+      field[block[i][0]][block[i][1]] = MOVING;
+    }
+    spawn_element(YELLOW);
+  }
+  
 }
 
 void loop() {
@@ -92,7 +156,6 @@ void setup() {
     }
   }
   
-  spawn_element(RED);
-  spawn_element(YELLOW);
+  spawn_element(GREY);
   sparkle.show();
 }
